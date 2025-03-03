c50daebfccea509b5de6c51e8fcef0d0124a8835
Moch Diki Widianto <dikiwidia@live.com>
2025-03-03 14:03:42 +0700

    update init task

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
+  <div class="flex flex-col items-center justify-center min-h-screen">
     <h1 class="text-3xl font-bold text-slate-600">Selamat Datang</h1>
+    <div class="flex items-center justify-center mt-4">
+      <a @click.prevent="" class="px-4 py-2 text-white bg-blue-500 rounded"
+        >Halaman Satu</a
+      >
+    </div>
   </div>
 </template>
