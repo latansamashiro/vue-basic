a66128a4951c708946209dd203d9f2c6bb48ba89
Mifta838 <miftapratikakhasan838@gmail.com>
2025-03-04 11:19:55 +0700

    declare reactive state

--- src/components/PageTwo.vue
+++ src/components/PageTwo.vue
@@ -1,3 +1,9 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const inputText = ref("")
+</script>
+
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <div
@@ -7,10 +13,11 @@
       <div class="flex items-center justify-center mt-4 flex-col gap-4">
         <input
           type="text"
-          class="px-4 py-2 w-1/3 text-white bg-slate-100 rounded outline-none focus:ring-2 focus:ring-blue-500"
+          v-model="inputText"
+          class="px-4 py-2 w-1/3 text-slate-600 bg-slate-100 rounded outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Masukkan Nama"
         />
-        <p class="text-md font-medium text-slate-600">Hasil:</p>
+        <p class="text-md font-medium text-slate-600">Hasil: {{ inputText }}</p>
         <p class="text-md font-medium text-slate-600">
           Tugas: Ketika mengetik di atas, tampilkan hasil nya secara dinamis,
           misal mengetikkan "Kucing", maka tampilkan "Kucing" sesuai dengan yang
